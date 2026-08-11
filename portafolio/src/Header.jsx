b515537ccac16790d0a2b08b35bc1bad4909476d
Angela Maria Garcia <angelag280598@gmail.com>
2026-08-10 21:10:05 -0500

    se agrefo informacion

---
+++ portafolio/src/Header.jsx
@@ -0,0 +1,14 @@
+export const Header = () => {
+  return(
+  <header>
+    <nav>
+      <ul>
+        <li><a href="">inicio</a></li>
+        <li><a href="">acerca de</a></li>
+        <li><a href="">contacto</a></li>
+        <li><a href="">FAQ</a></li>
+      </ul>
+    </nav>
+  </header>
+  )
+}
---
+++ portafolio/src/data/profile.js
@@ -0,0 +1,15 @@
+import fotoAngela from '../assets/fotoAMG.jpeg';
+
+export const profile = {
+    name: 'Angela Maria Garcia',
+    role: 'Desarrolladora Frontend',
+    tagline:'construyo interfaces rapidas y faciles de manejar',
+    photoUrl:'fotoAngela',
+    biography:'Aprendiz de Anális y Desarrollo de Software en el SENA. Me enfoco en arquitectura de componentes, buenas prácticas de React y accesibilidad web. ',
+    skills:['React', 'JavaScript', 'Git', 'Angular'],
+    social:[
+        { label:'GitHub', url: 'git@github.com:angelagg98/portafolio.git' },
+        { label:'Corre', url: 'mailto: angela280598@gmail.com'},
+    ]
+
+}
