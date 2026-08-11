c2efef9a8447bfdfcc6f3d10f8bb895a8299b06a
Angela Maria Garcia <angelag280598@gmail.com>
2026-08-10 21:41:12 -0500

    creacion de proyectos

---
+++ portafolio/src/data/projects.js
@@ -0,0 +1,21 @@
+
+export const projects = [
+
+    {
+    id: 'p1',
+    title: 'iniciacion con java',
+    description: 'documento donde se explica funcionalidades de java',
+    stack: ['JAVA'],
+    repoUrl: 'git@github.com:angelagg98/Repo_de_carlos.git',
+},
+
+{
+    id:'p2',
+    title: 'pagina',
+    description: 'pagina creada con Angular',
+    stack:['html', 'css','typeScrip', 'Angular'],
+    repoUrl:'git@github.com:angelagg98/angular-routing.git'
+}
+
+
+]
